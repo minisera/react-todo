cb7249715307c76d6e920da33beca505c3934e4d
koki otaka <67394974+minisera@users.noreply.github.com>
2021-09-07 09:37:27 +0900

    feat/完了のTODOのコンポーネント化

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./styles.css";
 import React, { useState } from "react";
 import { InputTodo } from "./components/InputTodo";
 import { IncompleteTodos } from "./components/IncompleteTodos";
+import { CompleteTodos } from "./components/CompleteTodos";
 
 export const App = () => {
   const [todoText, setTodoText] = useState("");
@@ -60,19 +61,7 @@ export const App = () => {
         onDelete={onClickDelete}
         onComplete={onClickComplete}
       />
-      <div className="complete-area">
-        <p className="title">完了のTODO</p>
-        <ul>
-          {completeTodos.map((todo, index) => {
-            return (
-              <div key={todo} className="list-row">
-                <li>{todo}</li>
-                <button onClick={() => onClickBack(index)}>戻す</button>
-              </div>
-            );
-          })}
-        </ul>
-      </div>
+      <CompleteTodos completeTodos={completeTodos} onClickBack={onClickBack} />
     </>
   );
 };
